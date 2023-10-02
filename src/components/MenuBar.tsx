0ab7bd97b6316e455507518e08f36b5cbf4ca5ad
musa <39286359+musabisimwa@users.noreply.github.com>
2023-10-02 18:23:36 -0400

    paths
    menu items are now clickable though no components are linked yet

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -1,16 +1,33 @@
 import { Menu } from "antd";
 import { HomeOutlined, UserOutlined, QuestionCircleOutlined } from '@ant-design/icons';
 
-export default function MenuBar(){
-    return ( <Menu mode="horizontal" theme="dark">
-    <Menu.Item key="home" icon={<HomeOutlined />}>
-      Home
-    </Menu.Item>
-    <Menu.Item key="profile" icon={<UserOutlined />}>
-      Profile
-    </Menu.Item>
-    <Menu.Item key="Questions" icon={<QuestionCircleOutlined />}>
-      Questions
-    </Menu.Item>
-  </Menu>);
+
+export default function MenuBar() {
+
+
+    return (<Menu mode="horizontal" theme="dark">
+
+        <Menu.Item
+            key="home"
+
+            icon={<HomeOutlined />}
+
+        > <a key="home" href="/">Home</a>
+
+        </Menu.Item>
+        <Menu.Item
+            key="profile"
+            icon={<UserOutlined />}
+        >
+            <a key="profile" href="/profile">Profile</a>
+            
+        </Menu.Item>
+        <Menu.Item
+            key="Questions"
+            icon={<QuestionCircleOutlined />}
+        ><a key="questions" href="/quiz"> Questions</a>
+           
+
+        </Menu.Item>
+    </Menu>);
 }
--- src/pages/QuizPage.tsx
+++ src/pages/QuizPage.tsx
@@ -6,15 +6,15 @@ import { useState } from "react";
 export default function QuizPage() {
     // data from quizReducer state
     //at submit qn/ generate success rate
-    const[loading,setLoading] = useState(false);
+    const [loading, setLoading] = useState(false);
 
     //submit the quiz state object then show modal of results
-    function onSubmit(){
-        
+    function onSubmit() {
+
         setLoading(!loading);
         console.log('sumit button clicked');
     }
-   
+
 
     return (
         <>
@@ -23,8 +23,8 @@ export default function QuizPage() {
                 <Col span={24}>
                     {
                         sampleQns.map(
-                            qn => <Question
-
+                            (qn, key) => <Question
+                                key={`${key}`}
                                 choices={qn.choices.choices}
                                 question={qn.qn}
                                 sucessRate={Math.round(Math.random() * 100)}
@@ -34,16 +34,16 @@ export default function QuizPage() {
                     }
                 </Col>
                 <Col span={12}>
-                    
-                        <Button 
-                        type="primary" 
+
+                    <Button
+                        type="primary"
                         loading={loading}
                         onClick={onSubmit}
-                        style={{marginTop:"60px"}}
-                        >
-                            {loading?'Submitting':'submit'}
-                        </Button>
-                    
+                        style={{ marginTop: "60px" }}
+                    >
+                        {loading ? 'Submitting' : 'submit'}
+                    </Button>
+
 
                 </Col>
             </Row>
